cc251cd65b77bd923e4334e27b8362949884c77f
Ankit Sisodiya <ankit@phppoets.com>
2017-03-16 17:34:54 +0530

    hello

--- service.php
+++ service.php
@@ -12,19 +12,26 @@ ini_set('max_execution_time', 100000);
 <style>
  
 .box > .icon { text-align: center; position: relative; }
-.box > .icon > .image { position: relative; z-index: 2; margin: auto; width: 66px; height: 66px; border: 8px solid white; line-height: 66px; border-radius: 50%; background:#837e7e; vertical-align: middle; }
+.box > .icon > .image { position: relative; z-index: 2; margin: auto; width: 66px; height: 66px; border: 8px solid #007787; line-height: 66px; border-radius: 50%; background:#837e7e; vertical-align: middle; background: #007787; }
 .box > .icon:hover > .image { background: #007787; }
-.box > .icon > .image > i { font-size: 36px !important; color: #fff !important; }
+.box > .icon > .image > i { font-size: 36px !important; color: #fff !important;  }
 .box > .icon:hover > .image > i { color: white !important; }
 .box > .icon > .info { margin-top: -24px; background:rgba(249, 249, 249, 0); border: 1px solid #e0e0e0; padding: 15px 0 10px 0; }
 .box > .icon:hover > .info { background: rgba(0, 0, 0, 0.04); border-color: #e0e0e0; color: white; }
-.box > .icon > .info > h3.title { font-family: "Roboto",sans-serif !important; font-size: 16px; color: #222; font-weight: 500; }
+.box > .icon > .info > h4.title { font-family: "Roboto",sans-serif !important; font-size: 17px; color: #0A4B7D;; font-weight: 700; }
 .box > .icon > .info > p { font-family: "Roboto",sans-serif !important; font-size: 13px; color: #666; line-height: 1.5em; margin: 20px;}
-.box > .icon:hover > .info > h4.title, .box > .icon:hover > .info > p, .box > .icon:hover > .info > .more > a { color: #222; }
-.box > .icon > .info > .more a { font-family: "Roboto",sans-serif !important; font-size: 12px; color: #222; line-height: 12px; text-transform: uppercase; text-decoration: none; }
+.box > .icon:hover > .info > h4.title, .box > .icon:hover > .info > p, .box > .icon:hover > .info > .more > a { color: #0A4B7D;  }
+.box > .icon > .info > .more a { font-family: "Roboto",sans-serif !important; font-size: 12px; color: #222; line-height: 12px; text-transform: uppercase; text-decoration: none; color: #fff; padding: 6px 8px; background-color: #007787; }
 .box > .icon:hover > .info > .more > a { color: #fff; padding: 6px 8px; background-color: #007787; }
 
 .box .space { height: 30px; }
+a {
+ outline: none;
+    text-decoration: none;
+    color: #72afd2;
+	    color: #23527c;
+    text-decoration: underline;	
+}
 </style>
 
 <div class="content-wrapper">
