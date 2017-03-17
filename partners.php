a680a3459165d07b599be72af81bf12bcfd0be8b
lakshitlohar <lakshitlohar7492@gmail.com>
2017-03-17 11:45:33 -0400

    partner

---
+++ partners.php
@@ -0,0 +1,156 @@
+<?php 
+	include("database.php");
+	include("header.php");
+	
+date_default_timezone_set('Asia/Calcutta');
+ini_set('max_execution_time', 100000);
+@$session_id=$_SESSION['id']; 
+  
+?>
+<div class="content-wrapper">
+ <section class="content-header">
+  <center>    <h1>
+     Our Partners
+   </h1></center>
+     </section>
+	 
+<section class="content">
+<div class="box box-default" style="background:white;">
+         
+         
+        <div class="box-body" style="background:white; margin-left:12px;">
+          <div class="row">
+		  </br>
+            <div class="col-md-3">
+              <div class="form-group">
+              <center><img src="images/partner/1.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			  <center><img src="images/partner/2.png" height="100px;"></center>
+ 
+              </div>
+             </div>
+			  
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/4.png" height="100px;"></center>
+              </div>
+             </div>
+			 
+			 <div class="col-md-3">
+              <div class="form-group">
+              <center><img src="images/partner/5.jpg" height="100px;"></center>
+              </div>
+             </div>
+			  
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/7.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/8.png" height="100px;"></center>
+              </div>
+             </div>
+			 
+			 <div class="col-md-3">
+              <div class="form-group">
+              <center><img src="images/partner/9.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			  <center><img src="images/partner/10.png" height="100px;"></center>
+ 
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/11.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/12.png" height="100px;"></center>
+              </div>
+             </div>
+			 
+			 <div class="col-md-3">
+              <div class="form-group">
+              <center><img src="images/partner/13.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			  <center><img src="images/partner/14.png" height="100px;"></center>
+ 
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/15.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/16.png" height="100px;"></center>
+              </div>
+             </div>
+			 
+			 
+			 <div class="col-md-3">
+              <div class="form-group">
+              <center><img src="images/partner/17.jpg" height="100px;"></center>
+              </div>
+             </div>
+			  
+			  
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/20.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 
+			  
+			 <div class="col-md-3">
+              <div class="form-group">
+			  <center><img src="images/partner/22.jpg" height="100px;"></center>
+ 
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/23.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 <div class="col-md-3">
+              <div class="form-group">
+			<center><img src="images/partner/24.jpg" height="100px;"></center>
+              </div>
+             </div>
+			 
+			 <div class="col-md-3">
+              <div class="form-group">
+              <center><img src="images/partner/25.jpg" height="100px;"></center>
+              </div>
+             </div>
+			  
+			 
+			 
+			</div>
+           
+        </div>
+         
+         
+      </div>
+
+</section>
+</div>
+
+
+<?php 
+include("footer.php");
+?>
