49d53942ba403342ecc9caad68e7f918e81589a5
lakshitlohar <lakshitlohar7492@gmail.com>
2017-03-20 13:16:37 -0400

    gg

--- index.php
+++ index.php
@@ -53,26 +53,49 @@ include('header.php');
 		<div id="team">
 			<div class="container">
 				<div class="row">
-                	<div class="col-md-6">
+                	<div class="col-md-12">
                         <div class="col-md-12">
                             <h2 class="wow bounce">Initiated By</h2>
                         </div>
-                        <div class="col-md-12 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
+                        <div class="col-md-2 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
                             <img src="images/logo/6.png" style="height:120px !important"  class="img-responsive" alt="team img">
                         </div>
+						<div class="col-md-2 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
+                            <img src="images/logo/logo.png" style="height:120px !important"  class="img-responsive" alt="team img">
+                        </div>
+						<div class="col-md-2 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
+                            <img src="images/logo/9.png" style="height:120px !important"  class="img-responsive" alt="team img">
+                        </div>
+                         <div class="col-md-2 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
+                            <img src="images/logo/1.jpg" style="height:120px !important"  class="img-responsive" alt="team img">
+                        </div>
+						<div class="col-md-2 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
+                            <img src="images/logo/3.png" style="height:120px !important"  class="img-responsive" alt="team img">
+                        </div> 
+						<div class="col-md-2 col-sm-12 wow fadeIn" data-wow-delay="0.3s">
+                            <img src="images/logo/2.jpg" style="height:120px !important"  class="img-responsive" alt="team img">
+                        </div>
+						
                          
+						 
                      </div>
-                     <div class="col-md-6">
+					 </div>
+					 
+					 <div class="row">
+                     <div class="col-md-12">
                      	<div class="col-md-12">
                             <h2 class="wow bounce">Supported By</h2>
                         </div>
-                        <div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.3s">
-                           <a target="_blank" href="http://www.rotaryclubmewar.org/"><img src="images/logo/logo.png" style="height:120px !important" class="img-responsive" alt="team img"></a>  
-                        </div>
+                         
                         <div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.6s">
                            
                             <a target="_blank" href="http://www.fusionoutsourcing.com/"><img src="images/logo/logo-fusion.png" style="height:120px !important"  class="img-responsive" alt="team img"></a>
                         </div>
+						 <div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.6s">
+                           
+                            <a target="_blank" href="#"><img src="images/logo/2.jpg" style="height:120px !important"  class="img-responsive" alt="team img"></a>
+                        </div>
+						 
                         <div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.9s">
                             <a target="_blank" href="http://phppoets.com/"><img src="images/logo/phppoetslogo.png"  style="margin-top:15px" class="img-responsive" alt="team img"></a>
                         </div>
