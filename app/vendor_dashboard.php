1e8db3e00deab448187ac79197a2c7dc0489ddae
Ankit Sisodiya <ankit@phppoets.com>
2017-03-20 16:18:22 +0530

    dasdad

--- app/vendor_dashboard.php
+++ app/vendor_dashboard.php
@@ -2,6 +2,29 @@
 include('auth.php'); 
 include("../config.php");
 include("header.php");
+  
+ 
+//--	LEAD OPEN
+$leadNew="SELECT `id` from `booking` where `master_sub_service_id` = '$SESSION_SUBSERVIDE' && `master_status` = '0'";
+$Newlead=mysql_query($leadNew);
+$lead_new=mysql_num_rows($Newlead);
+//--	LEAD OPEN
+$leadtransfer="SELECT `id` from `booking` where `master_sub_service_id` = '$SESSION_SUBSERVIDE' && `master_status` = '1'";
+$teanlead=mysql_query($leadtransfer);
+$lead_transfer=mysql_num_rows($teanlead);
+//--	LEAD OPEN
+$leadreject="SELECT `id` from `booking` where `master_sub_service_id` = '$SESSION_SUBSERVIDE' && `master_status` = '2'";
+$Openreject=mysql_query($leadreject);
+$lead_reject=mysql_num_rows($Openreject);
+//--	LEAD OPEN
+$leadconmpleted="SELECT `id` from `booking` where `master_sub_service_id` = '$SESSION_SUBSERVIDE' && `master_status` = '3'";
+$donelead=mysql_query($leadconmpleted);
+$lead_complete=mysql_num_rows($donelead);
+
+
+ //date('Ymd');
+ //echo date('hmsttt')
+//2016 06 23 13 23 59 958
 ?>
  
  <div class="content-wrapper">
@@ -12,47 +35,18 @@ include("header.php");
           <div class="box box-primary">
             <div class="box-footer no-padding">
               <div class="mailbox-controls" style="min-height: 470px;">
-			  <!---		TABS	---->
+			   
+				<!---		TABS	---->
 				<div class="col-lg-3 col-xs-6">
 				  <div class="small-box bg-aqua">
 					<div class="inner">
-					  <h3>150</h3>
-					  <p>Lead Open</p>
-					</div>
-					<div class="icon">
-					  <i class="fa fa-shopping-cart"></i>
-					</div>
-					<a href="lead_status.php?s=2" class="small-box-footer">
-					  More info <i class="fa fa-arrow-circle-right"></i>
-					</a>
-				  </div>
-				</div>
-				<!---		TABS	---->
-				<div class="col-lg-3 col-xs-6">
-				  <div class="small-box bg-green">
-					<div class="inner">
-					  <h3>53 </h3>
+					  <h3> <?php echo $lead_new; ?> </h3>
 					  <p>New Leads</p>
 					</div>
 					<div class="icon">
 					  <i class="ion ion-stats-bars"></i>
 					</div>
-					<a href="lead_status.php?s=1" class="small-box-footer">
-					  More info <i class="fa fa-arrow-circle-right"></i>
-					</a>
-				  </div>
-				</div>
-				<!---		TABS	---->
-				<div class="col-lg-3 col-xs-6">
-				  <div class="small-box bg-yellow">
-					<div class="inner">
-					  <h3>44</h3>
-					  <p>Leads Rejects</p>
-					</div>
-					<div class="icon">
-					  <i class="ion ion-person-add"></i>
-					</div>
-					<a href="lead_status.php?s=4" class="small-box-footer">
+					<a href="lead_status.php?s=0" class="small-box-footer">
 					  More info <i class="fa fa-arrow-circle-right"></i>
 					</a>
 				  </div>
@@ -61,11 +55,41 @@ include("header.php");
 				<div class="col-lg-3 col-xs-6">
 				  <div class="small-box bg-red">
 					<div class="inner">
-					  <h3>65</h3>
+					  <h3><?php echo $lead_reject; ?></h3>
+					  <p>Leads Rejects</p>
+					</div>
+					<div class="icon">
+					  <i class="ion ion-person-add"></i>
+					</div>
+					<a href="lead_status.php?s=2" class="small-box-footer">
+					  More info <i class="fa fa-arrow-circle-right"></i>
+					</a>
+				  </div>
+				</div>
+				<!---		TABS	---->
+				<div class="col-lg-3 col-xs-6">
+				  <div class="small-box bg-yellow">
+					<div class="inner">
+					  <h3><?php echo $lead_transfer; ?></h3>
 					  <p>Leads Transfer</p>
 					</div>
 					<div class="icon">
 					  <i class="ion ion-pie-graph"></i>
+					</div>
+					<a href="lead_status.php?s=1" class="small-box-footer">
+					  More info <i class="fa fa-arrow-circle-right"></i>
+					</a>
+				  </div>
+				</div>
+                <!---		TABS	---->
+				<div class="col-lg-3 col-xs-6">
+				  <div class="small-box bg-green">
+					<div class="inner">
+					  <h3> <?php echo $lead_complete; ?> </h3>
+					  <p>Completed Leads</p>
+					</div>
+					<div class="icon">
+					  <i class="ion ion-stats-bars"></i>
 					</div>
 					<a href="lead_status.php?s=3" class="small-box-footer">
 					  More info <i class="fa fa-arrow-circle-right"></i>
