87eaa8fdb5cbf9caff7f03ecb85fa1bb7279dc0a
Ankit Sisodiya <ankit@phppoets.com>
2017-03-24 10:20:30 +0530

    xa

--- ajax_page.php
+++ ajax_page.php
@@ -6,7 +6,7 @@ if($function_name=='mobileNo_check')
 	$mobile_no=$_GET['mobile'];
 	$ftc_date=mysql_query("select `mobile_no` from `register` where `mobile_no`= '$mobile_no'");
 	echo  $count= mysql_num_rows($ftc_date); 
-}
+} 
 
 
 
