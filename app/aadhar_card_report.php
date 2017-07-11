721cec1845bb53d9e38a2f7cd6446fe096f1eb03
anilgurjar <anilgurjer371@gmail.com>
2017-07-11 18:50:33 +0530

    ftyu

--- app/aadhar_card_report.php
+++ app/aadhar_card_report.php
@@ -18,7 +18,26 @@ $session_id=$_SESSION['SESSION_ID'];
 				<h3 class="box-title">AAdhar Card Report</h3>
 			</div>
 			<!------		Button 	----->
-			<div class="col-md-12" align="center">
+			<div class="row">
+			<div class="col-md-12">
+			<div class="col-md-6 col-sm-12"><h3>Records</h3>
+							<div class="dataTables_length" id="sample_1_length">
+							<select name="sample_1_length" aria-controls="sample_1" style="width:161px "class="form-control select2me find_records" id="find_records">
+                                <option value="10">Select</option>
+                                <option value="10">10</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                                <option value="100">100</option>
+                                <option value="150">150</option>
+                                <option value="200">200</option>
+                                <option value="300">300</option>
+                                <option value="500">500</option>
+                                <option value="1000">1000</option>
+                                <option value="2000">2000</option>
+                                <option value="3000">3000</option>
+							</select> </div></div>
+			
+			<div class="col-md-6" >
 				
 					<h3>AAdhar Card report</h3>
 					<div class="input-group">
@@ -38,13 +57,15 @@ $session_id=$_SESSION['SESSION_ID'];
 							?>
 						</select>
 					</div>
-				
+				</div>
+				</div>
+				</div>
 					<!-- /.box-header -->
 				<div class="box-body" id="data">
 					
 				</div>
 				<!-- /.box-body -->
-			</div>
+			
 			<!-- /.box -->
 		</div>
 		<!-- /.col -->
@@ -52,6 +73,20 @@ $session_id=$_SESSION['SESSION_ID'];
 	<!-- /.row -->
 </div>
 <script src="../assest/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script>
+$(document).ready(function(){    
+        $(".find_records").on("change",function(){
+			
+	    var view_u=$(".find_records option:selected").val();
+		
+	  	$.ajax({
+			url: "ajax_view_vendor.php?view_u="+view_u,
+			}).done(function(response) {
+		   $("#data").html(""+response+"");
+			});
+});
+});
+</script>
 <script>
 
  
