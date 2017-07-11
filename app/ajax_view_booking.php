1db40696b8f4a3d2edf7dc67a6a9d702ae7ad54f
anilgurjar <anilgurjer371@gmail.com>
2017-07-11 18:41:56 +0530

    hfjkdsh

---
+++ app/ajax_view_booking.php
@@ -0,0 +1,74 @@
+<?php
+include('auth.php'); 
+include("../config.php");
+$user=$_SESSION['category'];
+$view_u=$_GET['view_u'];
+@$SESSION_SUBSERVIDE=$_SESSION['SESSION_SUBSERVICE'];
+@$SESSION_USERTYPE=$_SESSION['SESSION_USERTYPE']; 
+?>
+<div align="right">
+<a class="btn btn-success " href="excel_view_report.php?id=<?php echo $pon; ?>"> Download Excel</a>
+</div>
+<table id="example1" class="table table-bordered table-striped">
+	<thead>
+		<tr>
+			<th>Sr.No</th>
+			<th>UdCare No</th>
+			<th>Code</th>
+			<th>Name</th>
+			<th>Email</th>
+			<th>Address</th>
+			<th>Mobile No</th>
+			<th>Master Service Id</th>
+			<th>Master Sub Service Id</th>
+			
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			
+			$Newlead=mysql_query("SELECT * from `booking` order by id ASC LIMIT $view_u");
+			$i=0;
+			while($lead_new=mysql_fetch_array($Newlead))
+			{
+				$i++;
+				$id=$lead_new['id'];	
+				$udcare_no=$lead_new['udcare_no'];	
+				$code=$lead_new['code'];	
+				$name=$lead_new['name'];	
+				$email=$lead_new['email'];	
+				$address=$lead_new['address'];
+				$mobile_no=$lead_new['mobile_no'];
+				$master_service_id=$lead_new['master_service_id'];
+						if(!empty($master_service_id))
+						{ 
+							$sql=mysql_query("select service_name from `master_services` where `id`='$master_service_id'");
+							$ftc=mysql_fetch_array($sql);
+							$service_name=$ftc['service_name'];
+						}
+						else{$service_name='';}
+				$master_sub_service_id=$lead_new['master_sub_service_id'];
+						if(!empty($master_sub_service_id)){
+						$sql1=mysql_query("select sub_services_name from `master_sub_services` where `id`='$master_sub_service_id'");
+						$ftc1=mysql_fetch_array($sql1);
+						$sub_services_name=$ftc1['sub_services_name'];
+						}
+						else
+						{$sub_services_name='';}
+		?>
+		<tr>
+			<td><?php echo $i++;?> </td>
+			<td><?php echo $udcare_no; ?></td>
+			<td><?php echo $code; ?></td>
+			<td><?php echo $name; ?></td>
+			<td><?php echo $email; ?></td>
+			<td><?php echo $address; ?></td>
+			<td><?php echo $mobile_no; ?></td>
+			<td><?php echo $service_name; ?></td>
+			<td><?php echo $sub_services_name; ?></td>
+		</tr>
+	   <?php } ?>
+	</tbody>
+</table>
+
+
---
+++ app/ajax_view_member.php
@@ -0,0 +1,123 @@
+<?php
+include('auth.php'); 
+include("../config.php");
+include('function.php');
+$user=$_SESSION['category'];
+$view_u=$_GET['view_u'];
+$p=$_SESSION['SESSION_ID'];
+$session_id=$_SESSION['SESSION_ID'];
+
+if(isset($_POST['deleted'])){
+	$reason_for_delete=$_POST['reason_for_delete'];
+	$update_id=$_POST['update_id'];
+	mysql_query("update `register` set `reason_for_delete`='$reason_for_delete',`flag`='1' where `id`='$update_id' ");
+}  
+ ?>
+ 
+ <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+            <div class="box col-md-12" style="background:white;">
+            <div class="box-header">
+              <h3 class="box-title"> User Report </h3>
+			</br>
+            </div>
+			
+            <div class="box-body">
+				<div align='right' colspan='7'><a class="btn btn-success" href="member_excel_report.php?id=<?php echo $p; ?>"><b>Download Excel</b>&nbsp; &nbsp;<i class="fa fa-cloud-download "></i></a></div><br>
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+				
+                <tr>
+					
+					<th>S/No.</th>
+					<th>Name</th>
+					<th>DOB</th>
+                    <th>Age</th>
+ 					<th>Mobile No.</th>
+					<th>Identity proof</th>
+					<th>Action</th> 
+                </tr>
+                </thead>
+                <tbody>
+				<?php
+			  $r1=mysql_query("select * from register where flag='0' order by id ASC LIMIT 0,$view_u");							
+					$i=0;
+					while($row1=mysql_fetch_array($r1))
+					{
+						$i++;
+						$id=$row1['id'];
+						$name=$row1['name'];
+						$dob=$row1['dob'];
+						$identity_proof=$row1['identity_proof'];
+						$email_id=$row1['email_id'];
+						$mobile_no=$row1['mobile_no'];
+						$name=decode($name,'UDRENCODE');
+						$email_id=decode($email_id,'UDRENCODE');
+					 
+						$mobile_no=decode($mobile_no,'UDRENCODE');
+						if(!empty($dob))
+						{
+							$dateOfBirth = date('d-m-Y',strtotime($dob));
+							$today = date("Y-m-d");
+							$diff = date_diff(date_create($dateOfBirth), date_create($today));
+							$age_year=$diff->format('%y');  
+							$month_year=$diff->format('%m'); 
+							$day_year=$diff->format('%d'); 
+						}	
+							
+						?>
+						
+                    <tr>
+                        <td><?php echo $i;?></td>
+                        <td><?php echo $name;?></td>
+                        <td><?php echo $dob;?></td>
+                        <td><?php if(!empty($dob)) {  echo $age_year.' Year and '.$month_year.' Month'; }?></td>
+                         <td> <?php echo $mobile_no;?></td>
+						  <td><img src="../identity_proof/<?php echo $identity_proof;?>" width="100px" height="100px"/> </td>
+                        <td>
+                            <a href="edit_member.php?ati_utf_id=<?php echo $row1['id']; ?>" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> </a>
+                            
+                            <a data-toggle="modal" data-target="#delete<?php echo $row1['id']; ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> </a>
+                             <div class="modal fade" id="delete<?php echo $row1['id']; ?>" role="dialog">
+                                <div class="modal-dialog">
+                                  <div class="modal-content">
+                                  <form method="post">
+                                    <div class="modal-header">
+                                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                      <h4 class="modal-title">Do you want to delete this member </h4>
+                                    </div>
+                                    <div class="modal-body"  style="min-height:100px">
+                                        <div class="form-group col-md-12">
+                                         <label class="">Reason for delete</label>
+                                         <input type="hidden" name="update_id" value="<?php echo $row1['id']; ?>" />
+                                         <textarea name="reason_for_delete" class="form-control" cols="70" style="resize:none;" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                      <button type="submit" name="deleted" class="btn btn-danger">Delete</button>
+                                    </div>
+                                  </form>
+                                  </div>
+                                </div>
+                              </div>
+                        </td>
+                    </tr>
+              <?php } ?>
+                </tbody>
+                 
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+                    </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
+                 
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+ </div>
