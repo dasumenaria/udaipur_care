ca955e6459bcf5e87e038d557457e539cf7617ae
lakshitlohar <lakshitlohar7492@gmail.com>
2017-07-13 17:59:36 +0530

    gggg
    kjjkhk

--- u_d_c.php
+++ u_d_c.php
@@ -1,153 +1,182 @@
 <?php
 include('header.php');
 include('config.php');
-?>  
+?>
+
 <style>
-.serviceBox{
-    background: hsl(0, 0%, 100%) none repeat scroll 0 0;
-    border: 1px solid hsl(0, 0%, 82%);
-    box-shadow: 5px 5px 0 hsla(0, 0%, 0%, 0.07);
-    padding: 20px;
-}
-.serviceBox .service-icon{
-    float: left;
-}
-.serviceBox .service-content h3{
-    margin: 11px 0 30px 67px;
-    color: #000;
-    font-size: 16px;
-    font-weight: 600;
-}
-.serviceBox .service-content p{
-    font-size: 13px;
-    line-height: 22px;
-    margin: 0;
-}
-.serviceBox .service-icon i{
-    background: #e74c3c;
-    box-shadow: 5px 5px 0 hsla(0, 0%, 0%, 0.1);
-    color: hsl(0, 0%, 100%);
-    font-size: 26px;
-    font-weight: 600;
-    height: 50px;
-    line-height: 50px;
-    text-align: center;
-    width: 50px;
-    transition:all 0.2s ease-in-out;
-}
-.serviceBox:hover .service-icon i{
-    background: hsl(208, 17%, 20%) none repeat scroll 0 0;
-    border-radius: 50%;
-    transform: translate(0px, 3px);
-    transition:all 0.5s ease-in-out;
-}
-@media only screen and (max-width: 990px){
-    .serviceBox{
-        margin-bottom: 20px;
-    }
+ 
+ 
+.content {
+    padding: 30px 0;
 }
 
+/***
+Pricing table
+***/
+.pricing {
+  position: relative;
+  margin-bottom: 15px;
+  border: 2px solid #33bacc;
+}
 
+.pricing-active {
+  border: 2px solid #36d7ac;
+  margin-top: -10px;
+  box-shadow: 7px 7px rgba(54, 215, 172, 0.2);
+}
 
+.pricing:hover {
+  border: 2px solid #007787;
+}
 
+.pricing:hover h4 {
+  color: #36d7ac;
+} 
 
+.pricing-head {
+  text-align: center;
+}
 
-</style>		
-		<div id="service">
-	 <div class="container">
-	 </br>
-    <div class="row">
-         <div class="col-md-4">
-            <div class="serviceBox">
-                <div class="service-icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="service-content">
-                    <h3>Spicysigdi</h3>
-                    <p>
-                       <>
-                    </p>
-                </div>
-            </div>
-        </div>
-		<div class="col-md-4">
-            <div class="serviceBox">
-                <div class="service-icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="service-content">
-                    <h3>Web Development</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus vitae dui efficitur facilisis non vitae metus. Phasellus.
-                    </p>
-                </div>
-            </div>
-        </div>
-		<div class="col-md-4">
-            <div class="serviceBox">
-                <div class="service-icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="service-content">
-                    <h3>Web Development</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus vitae dui efficitur facilisis non vitae metus. Phasellus.
-                    </p>
-                </div>
-            </div>
-        </div>
-	</div>	
-	</br></br></br>
-	 <div class="row">
-         <div class="col-md-4">
-            <div class="serviceBox">
-                <div class="service-icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="service-content">
-                    <h3>Web Development</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus vitae dui efficitur facilisis non vitae metus. Phasellus.
-                    </p>
-                </div>
-            </div>
-        </div>
-		<div class="col-md-4">
-            <div class="serviceBox">
-                <div class="service-icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="service-content">
-                    <h3>Web Development</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus vitae dui efficitur facilisis non vitae metus. Phasellus.
-                    </p>
-                </div>
-            </div>
-        </div>
-		<div class="col-md-4">
-            <div class="serviceBox">
-                <div class="service-icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="service-content">
-                    <h3>Web Development</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus vitae dui efficitur facilisis non vitae metus. Phasellus.
-                    </p>
-                </div>
-            </div>
-        </div>
-	</div>
-</div>
-            </div>
-        </div>
-    </div>
+.pricing-head h3,
+.pricing-head h4 {
+  margin: 0;
+  line-height: normal;
+}
 
-</div>
+.pricing-head h3 span,
+.pricing-head h4 span {
+  display: block;
+  margin-top: 5px;
+  font-size: 14px;
+  font-style: italic;
+}
 
+.pricing-head h3 {
+  font-weight: 300;
+  color: #fafafa; 
+  font-size: 27px;
+  
+ /* border-bottom: solid 2px #e7e0e0;*/
+  
+}
+
+.pricing-head h4 {
+  color: #000;
+  padding: 5px 0;
+  font-size: 32px;
+  font-weight: 300;
+  background: #f3f3f3;
+  border-bottom: solid 2px #eee ;
+}
+
+.pricing-head-active h4 {
+  color: #36d7ac;
+}
+
+.pricing-head h4 i {
+  top: -6px;
+  font-size: 18px;
+  font-style: normal;
+  position: relative;
+}
+
+.pricing-head h4 span {
+  top: -10px;
+  font-size: 14px;
+  font-style: normal;
+  position: relative;
+}
+
+/*Pricing Content*/
+.pricing-content li {
+  color: #888;
+  font-size: 12px;
+  padding: 7px 15px;
+  border-bottom: solid 1px #f5f9e7;
+}  
+
+/*Pricing Footer*/
+.pricing-footer {
+  color: #777;
+  font-size: 11px;
+ 
+  text-align: center;
+  padding: 0 20px 19px;
+}
+
+/*Priceing Active*/
+.price-active,
+.pricing:hover {
+  z-index: 9;
+}
+
+.price-active h4 {
+  color: #36d7ac;
+}
+
+.no-space-pricing .pricing:hover {
+  transition: box-shadow 0.2s ease-in-out;
+}
+
+.no-space-pricing .price-active .pricing-head h4,
+.no-space-pricing .pricing:hover .pricing-head h4 {
+  color: #36d7ac;
+  padding: 15px 0;
+  font-size: 80px;
+  transition: color 0.5s ease-in-out;
+}
+
+.yellow-crusta.btn {
+  color: #FFFFFF;
+  background-color:  #007787; 
+}
+.yellow-crusta.btn:hover,
+.yellow-crusta.btn:focus,
+.yellow-crusta.btn:active,
+.yellow-crusta.btn.active {
+    color: #FFFFFF;
+    background-color: #2191a1;
+}
+  
+   
+</style> 
+  
+ 
+<section class="content-header">
+       <center>
+      	<h2 class="wow bounce" style=" padding-top:60px; color:#707568"> <strong> 
+		DISCOUNT 
+       </strong></h2>
+       </center>
+	   </section>
+ 
+  <div id="service" style="padding: 22px 61px; " class="footer_fix">
+		 
+		 
+   <div class="container content">
+	<div class="row"> 
+		<!-- Pricing -->
+		 <div class="col-md-3">
+			<div class="pricing hover-effect">
+				<div class="pricing-head">
+					<h3><img src="images/partner/discount/7.jpg" width="100%" height="150px"></h3>
+					  
+				</div>
+				 <div class="pricing-footer">
+					<p>For Discount Click here <span class="label label-sm label-warning"  ><a href="service_booking.php" style="font-size:12px;color:white;text-decoration:none;">
+						Get Coupan
+					</a><i class="fa fa-share " style="height:10px"></i></span>
+					</p>
+					
+				</div>
+			</div>
 		</div>
-		</br>
+	 	  
+	 </div>
+</div> 
+</div>
+     
+		 
 		<!-- end service -->
 
 <?php
